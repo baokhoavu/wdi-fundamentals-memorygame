2429293e22df57987c4fbf1543b1a378713f3f5b
baokhoavu <tealkrysta92@gmail.com>
2016-11-28 01:37:47 -0800

    Update main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,15 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var CardFour = "King";
+
+If (cardTwo === 0 && cardFour === 0){
+}	else if (King) {
+	print "You found a match!"
+}	else if (Queen) {
+	print "You found a match!"
+}	else {
+	print "Sorry, try again"
+}
+
+function createCards
